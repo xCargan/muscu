3b5ddca0a382f151edf4656cb0de26e12f5e2daf
xCargan <lgc.carlier@gmail.com>
2025-04-03 22:21:45 +0200

    Ajout du fichier .gitignore pour ignorer node_modules

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Test from './components/test/Test';
+
+const App = () => {
+     return (
+          <div>
+               <Test />
+          </div>
+     );
+};
+
+export default App;     
---
+++ frontend/src/components/test/Test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Test = () => {
+     return (
+          <div>
+               <p>Ceci est un test composent</p>
+          </div>
+     );
+};
+
+export default Test;
